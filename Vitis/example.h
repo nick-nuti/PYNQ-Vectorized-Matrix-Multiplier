2b931f8982eab15d8b6756b193526bbc0851de27
Nicholas Nuti <54613418+nick-nuti@users.noreply.github.com>
2022-11-13 20:57:01 -0800

    Delete Vitis directory

--- Vitis/example.h
+++
@@ -1,4 +0,0 @@
-#include "ap_int.h"
-#include "hls_burst_maxi.h"
-
-void transfer_kernel(hls::burst_maxi<float> matA, hls::burst_maxi<float> matB,hls::burst_maxi<float> matout, const int mat1r, const int mat1cmat2r, const int mat2c);
